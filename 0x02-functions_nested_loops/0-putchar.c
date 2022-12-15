703cb1ec07a1662fe982e10cda8be5c8828b8f04
karimi-george <kimamogeorge69@gmail.com>
2022-12-15 01:07:48 -0800

    first push

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - entry point
+ *
+ * prints out_putchar followed by a newline.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char ch[8] = "_putchar";
+	int m;
+
+	for (m = 0; m < 8; m++)
+		_putchar(ch[m]);
+	_putchar('\n');
+	return (0);
+}
